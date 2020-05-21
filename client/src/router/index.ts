ac6917b66c5b39dce7a8cc9f6d029c126cc92d26
Nico Lehmann <nico.lehmannm@gmail.com>
2020-05-20 20:46:29 -0700

    Add SignIn View

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,14 +1,20 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import SignIn from "../views/SignIn.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
+    path: "/sign_in",
+    name: "SignIn",
+    component: SignIn,
+  },
+  {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
@@ -17,14 +23,14 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
---
+++ client/src/views/SignIn.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="signIn">
+    <h1>SignIn</h1>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class SignIn extends Vue {}
+</script>
