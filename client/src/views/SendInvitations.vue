aaa34132048602e79cd4b490f3966b7cd263a8a5
Nico Lehmann <nico.lehmannm@gmail.com>
2020-05-28 10:25:52 -0700

    Add navbar to invitations

--- client/src/views/SendInvitations.vue
+++ client/src/views/SendInvitations.vue
@@ -1,48 +1,52 @@
 <template>
-  <b-form class="form-send-invitations text-center" @submit="onSubmit">
-    <b-container>
-      <b-row class="mb-3">
-        <b-col sm>
-          <span class="font-weight-bold">Email address</span>
-        </b-col>
-        <b-col sm>
-          <span class="font-weight-bold">Full name</span>
-        </b-col>
-      </b-row>
-      <b-row v-for="(item, index) in rows" v-bind:key="index" class="mb-3">
-        <b-col sm>
-          <b-form-input
-            type="email"
-            v-model="item.emailAddress"
-            required
-          ></b-form-input>
-        </b-col>
-        <b-col sm>
-          <b-form-input
-            type="text"
-            v-model="item.fullName"
-            required
-          ></b-form-input>
-        </b-col>
-      </b-row>
-      <div clas="d-flex">
-        <b-button size="lg" variant="light" class="btn-plus" v-on:click="add">
-          <b-icon-plus-circle />
+  <div>
+    <navbar />
+    <b-form class="form-send-invitations text-center" @submit="onSubmit">
+      <b-container>
+        <b-row class="mb-3">
+          <b-col sm>
+            <span class="font-weight-bold">Email address</span>
+          </b-col>
+          <b-col sm>
+            <span class="font-weight-bold">Full name</span>
+          </b-col>
+        </b-row>
+        <b-row v-for="(item, index) in rows" v-bind:key="index" class="mb-3">
+          <b-col sm>
+            <b-form-input
+              type="email"
+              v-model="item.emailAddress"
+              required
+            ></b-form-input>
+          </b-col>
+          <b-col sm>
+            <b-form-input
+              type="text"
+              v-model="item.fullName"
+              required
+            ></b-form-input>
+          </b-col>
+        </b-row>
+        <div clas="d-flex">
+          <b-button size="lg" variant="light" class="btn-plus" v-on:click="add">
+            <b-icon-plus-circle />
+          </b-button>
+        </div>
+        <b-button variant="primary" size="lg" type="submit" class="mt-4">
+          Send
         </b-button>
-      </div>
-      <b-button variant="primary" size="lg" type="submit" class="mt-4">
-        Send
-      </b-button>
-    </b-container>
-  </b-form>
+      </b-container>
+    </b-form>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { Invitation } from "../models";
-import ApiService from "../services/api";
+import { Invitation } from "@/models";
+import ApiService from "@/services/api";
+import Navbar from "@/components/Navbar.vue";
 
-@Component
+@Component({ components: { Navbar } })
 export default class SignIn extends Vue {
   rows: Invitation[] = [{ emailAddress: "", fullName: "" }];
 
