145581b42f981640158097ed4a13a1714c80466a
Rose Kunkel <rose@rosekunkel.me>
2020-06-02 08:17:42 -0700

    Fix indentation

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,22 +8,22 @@
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from "vue-property-decorator";
-  import Navbar from "@/components/Navbar.vue"
-  import router from "@/router";
-  import store from "@/store";
+import { Vue, Component } from "vue-property-decorator";
+import Navbar from "@/components/Navbar.vue"
+import router from "@/router";
+import store from "@/store";
 
-  @Component({
-    store,
-    router,
-    components: { Navbar }
-  })
-  export default class App extends Vue {}
+@Component({
+  store,
+  router,
+  components: { Navbar }
+})
+export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-  @import "~bootstrap/scss/bootstrap";
-  @import "~bootstrap-vue/src/index.scss";
+@import "~bootstrap/scss/bootstrap";
+@import "~bootstrap-vue/src/index.scss";
 
 // :root {
 //   --font-family-sans-serif: -apple-system, BlinkMacSystemFont, "Segoe UI",
@@ -38,8 +38,8 @@ body {
 body {
   background-color: #f5f5f5;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
-    "Segoe UI Emoji", "Segoe UI Symbol" !important;
+  "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+  "Segoe UI Emoji", "Segoe UI Symbol" !important;
   // font: 1rem/1.5 var(--font-family-sans-serif) !important;
 }
 
