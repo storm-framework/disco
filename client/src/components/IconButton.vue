bc10670445c09bdba6f10d284cc4e44c4d312317
Rose Kunkel <rose@rosekunkel.me>
2020-06-08 12:42:40 -0400

    Factor out icon buttons + accessibility fixes.

---
+++ client/src/components/IconButton.vue
@@ -0,0 +1,32 @@
+<template functional>
+  <b-button class="icon-button" v-bind="data.attrs" v-on="listeners">
+    <font-awesome-icon class="icon" :icon="props.icon" />
+    <slot/>
+  </b-button>
+</template>
+
+<script lang="ts">
+export default {
+  functional: true,
+  props: {
+    icon: {
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.icon-button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 100%;
+  height: 1.5em;
+  box-sizing: content-box;
+}
+
+.icon {
+  margin-right: .5em;
+}
+</style>
